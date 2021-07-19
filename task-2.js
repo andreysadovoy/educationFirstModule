54029e90121e37541721f15047e48850d76925a9
andreysadovoy <sadovoi.exceedteam@gmail.com>
2021-07-19 09:42:31 +0300

    task-2

--- task-2.js
+++ task-2.js
@@ -1,14 +1,13 @@
 // 3.1 Вернуть количество элементов массива.
-let array = [5,7,9,11,13,15];
+  let array = [5,7,9,11,13,15];
 
 TestFunction = (arr, val) => {
   array.forEach((value, index) => {
-  if(value === val){
-    console.log(value)
-  }
+    if(value === val){
+      console.log(value)
+    }
   });
 
 }
 
-const t = TestFunction(array);
-console.log(t);
+const t = TestFunction(array,5);
