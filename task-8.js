8c5a3ae8386485a5d15462f9e4426bcc801a053f
andreysadovoy <sadovoi.exceedteam@gmail.com>
2021-07-19 17:02:54 +0300

    task-8

---
+++ task-8.js
@@ -0,0 +1,37 @@
+// В функцию передаем коллекцию и 2 значения. По результату выполнения вывести все объекты массива, у которых одно свойство ('name' )равно первому параметру или другое свойство ('year') равно второму параметру.
+
+  let array = [12,24,38,64,128,364,425,1028]
+let collectionArr =[
+  {
+    name: "Ford",
+    model: "Mustang",
+    year: 1969
+  },
+  {
+    name: "Ferrari",
+    model: "Mustang",
+    year: 1969
+  },{
+    name: "BMW",
+    model: "Mustang",
+    year: 1969
+  },{
+    name: "Ford",
+    model: "Mustang",
+    year: 1990
+  }
+];
+
+const testFunction = (arr, val, val2) => {
+  arr.forEach(value =>{
+    if (value.name === val || value.year === val2) {
+      console.log(value)
+    }
+  });
+}
+
+
+
+
+const t = testFunction(collectionArr, 'Ford',1990);
+console.log(t);
