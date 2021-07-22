8d83ed9ff78732d94733a012229f7961f2f2c77b
andreysadovoy <sadovoi.exceedteam@gmail.com>
2021-07-22 11:49:10 +0300

    task-14

---
+++ task-14.js
@@ -0,0 +1,18 @@
+
+
+let array = [5, 8, 10, 105, 118, 234, 567, 90, 771]
+
+testFunction = (arr) => {
+  let max = -1;
+arr.forEach(value => {
+  if (value>max) {
+    max = value;
+  }
+})
+
+  return max
+}
+
+
+const t = testFunction(array);
+console.log(t)
