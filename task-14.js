92d465a2b1867da4f92648a4c0a18ce651dd30d8
andreysadovoy <sadovoi.exceedteam@gmail.com>
2021-07-22 11:50:17 +0300

    task-14

--- task-14.js
+++ task-14.js
@@ -5,7 +5,7 @@ let array = [5, 8, 10, 105, 118, 234, 567, 90, 771]
 testFunction = (arr) => {
   let max = -1;
 arr.forEach(value => {
-  if (value>max) {
+  if (value > max) {
     max = value;
   }
 })
